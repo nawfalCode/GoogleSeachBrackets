17f0bcd47e299f88cb42217414326a9eaa9cb2ca
Nawfal Mahdi <nawfal.mahdi@hotmail.com>
2015-06-05 22:57:06 +1000

    right click item has been added; linked to function; new tab is working; next extra documentation and testing

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
 define(function (require, exports, module) {
-            "use strict";
-            var CommandManager = brackets.getModule("command/CommandManager");
-            var Menus = brackets.getModule("command/Menus");
-            var EditorManager = brackets.getModule("editor/EditorManager");
+    "use strict";
+    var CommandManager = brackets.getModule("command/CommandManager");
+    var Menus = brackets.getModule("command/Menus");
+    var EditorManager = brackets.getModule("editor/EditorManager");
+    var right_click_search = "RightClickExtended.SearchHandler";
+    var AppInit = brackets.getModule("utils/AppInit");
 
+    //   var strings = require("strings");
+    /**
+        Logs into System Console
+        @param 
+        @Input: String represnts the message to be printed to console
+        @return None
+    */
+    var strings = {
+        RIGHT_CLICK_SEARCH_TITLE: "Search in Google",
+        USER_NOT_FOUND: 'Please Enter User Name',
+        TEAM_NOT_FOUND: 'Please Enter Team Directory Name',
+        SETTINGS_SHORTCUT: 'Ctrl-Shift-I',
+        SETTINGS_MENU_TITLE: 'ENG1003 Settings',
+        UPLOAD_SHORTCUT: 'Ctrl-Shift-U',
+        UPLOAD_NEMU_TITLE: 'ENG1003 Uploader',
+        STORAGE_KEY: 'Eng1003Uploader.Monash'
+    }
+    module.exports = strings;
 
+    function log(msg) {
 
-            var right_click_search = "RightClickExtended.SearchHandler";
-            var AppInit = brackets.getModule("utils/AppInit");
+        console.log('GoogleExt:' + msg);
+    }
 
-            var strings = require("strings");
-            /**
-                Logs into System Console
-                @param 
-                @Input: String represnts the message to be printed to console
-                @return None
-            */
+    /**
+        Handle the Right Click Event
+        @param 
+        @Input: None
+        @return None
+    */
+    function handleRightClickSearch() {
 
-            function log(msg) {
+        var thisEditor = EditorManager.getCurrentFullEditor();
+        var keyword = thisEditor._codeMirror.getSelection();
+        if (keyword.length > 0) { // if the user has selected (highlited) something
+            var url = "https://www.google.com.au/search?q=" + keyword;
+            brackets.app.openURLInDefaultBrowser(url);
+        }
+    }
 
-                console.log('GoogleExt:' + msg);
-            }
+    /**
+        System Initialization, Command Registratios, and event handlers
+    */
 
-            /**
-                System Initialization, Command Registratios, and event handlers
-            */
-
-            AppInit.appReady(function () {
-                        CommandManager.register(strings.RIGHT_CLICK_SEARCH_TITLE, right_click_search, handleRightClickSearch);
-                        Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
-                        Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(right_click_search);
-                    }
+    AppInit.appReady(function () {
+        CommandManager.register(strings.RIGHT_CLICK_SEARCH_TITLE, right_click_search, handleRightClickSearch);
+        Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
+        Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(right_click_search);
+    });
+});
--- strings.js
+++ strings.js
@@ -2,16 +2,3 @@
 System Strings
  */
 
-var strings = {
-    RIGHT_CLICK_SEARCH_TITLE: 'Search in Google';
-    USER_NOT_FOUND: 'Please Enter User Name',
-    TEAM_NOT_FOUND: 'Please Enter Team Directory Name',
-    SETTINGS_SHORTCUT: 'Ctrl-Shift-I',
-    SETTINGS_MENU_TITLE: 'ENG1003 Settings',
-    UPLOAD_SHORTCUT: 'Ctrl-Shift-U',
-    UPLOAD_NEMU_TITLE: 'ENG1003 Uploader',
-    STORAGE_KEY: 'Eng1003Uploader.Monash',
-
-
-};
-module.exports = strings;
